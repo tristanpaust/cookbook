c584685b7efee8bd5ec2844fa9cfe1dd6fe6da15
Tristan Paust <tristanpaust@gmail.com>
2019-09-12 17:36:14 +0200

    adding basic profile editing layout

--- frontend/components/Profile/Profile.jsx
+++ frontend/components/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../../global.jsx'
+import '../../css/Profile.css';
 
 export default class Profile extends Component {
   constructor() {
@@ -10,8 +11,18 @@ export default class Profile extends Component {
       isFetchingData: true,      
       message: 'Loading...',
       profile: {},
+      isPasswordOpen: false,
+      isUsernameOpen: false,
+      isEmailOpen: false
     }
 
+    this.openChangePassword = this.openChangePassword.bind(this);
+    this.openChangeUsername = this.openChangeUsername.bind(this);
+    this.openChangeEmail = this.openChangeEmail.bind(this);
+
+    this.changePassword = this.changePassword.bind(this);
+    this.changeUsername = this.changeUsername.bind(this);
+    this.changeEmail = this.changeEmail.bind(this);
   }
 
   async componentDidMount() {
@@ -27,6 +38,36 @@ export default class Profile extends Component {
     return new Promise((resolve) => {
       this.setState(state, resolve)
     });
+  }
+
+  openChangeEmail() {
+    this.setState({
+      isEmailOpen: !this.state.isEmailOpen
+    })
+  }
+  
+  openChangeUsername() {
+    this.setState({
+      isUsernameOpen: !this.state.isUsernameOpen
+    })
+  }
+
+  openChangePassword() {
+    this.setState({
+      isPasswordOpen: !this.state.isPasswordOpen
+    })
+  }
+
+  changeEmail() {
+
+  }
+
+  changeUsername() {
+
+  }
+
+  changePassword() {
+
   }
 
   createItems(item) {
@@ -63,7 +104,39 @@ export default class Profile extends Component {
         <div className="container-fluid">
           
           <h1>Hallo {this.state.profile.email}</h1>
-          <a>Profil bearbeiten</a>
+
+          <div className="row">
+            <p className="col"> Aktueller Benutzername: {this.state.profile.username}</p>
+            <p className="col profile-button" onClick={this.openChangeUsername}>Benutzername ändern</p>
+          </div>
+
+          <div className={this.state.isUsernameOpen ? 'row' : 'hidden'}>
+            <input type="text" placeholder="Neuer Benutzername" />
+            <button onClick={this.changeUsername}>Benutzername aktualisieren</button>
+          </div>
+
+          <div className="row">
+            <p className="col"> Aktuelle Email-Adresse: {this.state.profile.email}</p>
+            <p className="col profile-button" onClick={this.openChangeEmail}>Email-Adresse ändern</p>
+          </div>
+
+          <div className={this.state.isEmailOpen ? 'row' : 'hidden'}>
+            <input type="mail" placeholder="Neue Email-Adresse" />
+            <button onClick={this.changeEmail}>Email aktualisieren</button>
+          </div>
+
+          <div className="row">
+            <p className="col">a</p>
+            <p className="col profile-button" onClick={this.openChangePassword}>Passwort ändern</p>
+          </div>
+
+          <div className={this.state.isPasswordOpen ? 'row' : 'hidden'}>
+            <input type="password" placeholder="Altes Passwort" />
+            <input type="password" placeholder="Neues Passwort" />
+            <input type="password" placeholder="Neues Passwort wiederholen" />
+
+            <button onClick={this.changePassword}>Passwort aktualisieren</button>
+          </div>
 
           <h1>Favoriten</h1>
           <div className="row">
