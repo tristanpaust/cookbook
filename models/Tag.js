be53046d55d26e5f51e3bb3a4cc9442d6c9bbd8b
Tristan Paust <tristanpaust@gmail.com>
2020-01-27 18:39:59 +0100

    removed old files

--- models/Tag.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const TagSchema = new mongoose.Schema({
-  title: { type: String, required: true},
-});
-
-module.exports = mongoose.model('Tag', TagSchema);
